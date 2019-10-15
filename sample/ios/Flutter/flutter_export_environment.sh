353367c610528bf4ed7c883abf0bf04e8a910240
Vefik Fırat Akman <vfakman@gmail.com>
2019-10-15 22:33:49 +0300

    Git Settings

--- sample/ios/Flutter/flutter_export_environment.sh
+++ sample/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\src\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\vfakm\AndroidStudioProjects\gtubtmobile\gtubt-mobile\gtubt-mobile\sample"
+export "FLUTTER_APPLICATION_PATH=C:\Users\vfakm\AndroidStudioProjects\XXX\sample"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build\ios"
