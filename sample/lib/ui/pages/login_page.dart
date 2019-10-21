8ca3c2870e9dd4ea21b97d14b463713c628ccd61
Vefik Fırat Akman <vfakman@gmail.com>
2019-10-21 14:42:17 +0300

    feat: LoginPageUI has been changed according to the feedback and arrangements were made according to the dimensions in the design.

--- sample/lib/ui/pages/login_page.dart
+++ sample/lib/ui/pages/login_page.dart
@@ -2,159 +2,192 @@ import 'package:flutter/material.dart';
 import 'package:GTUBT/resources/colors.dart';
 
 class LoginPage extends StatefulWidget {
-    static String tag = 'login-page';
+  static String tag = 'login-page';
 
-    @override
-    _LoginPageState createState() => new _LoginPageState();
+  @override
+  _LoginPageState createState() => new _LoginPageState();
 }
 
 class _LoginPageState extends State<LoginPage> {
-    @override
-    Widget build(BuildContext context) {
-        final logo = Container(
-            height: 192,
-            width: 192,
-            child: Image.asset('assets/logo.png'),
-        );
-
-        final email = Container(
-            child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                    Container(
-                        margin: EdgeInsets.only(bottom: 2),
-                        child: Text(
-                            'Email',
-                            textAlign: TextAlign.left,
-                            style: TextStyle(
-                                color: Colors.white,
-                                fontWeight: FontWeight.w800,
-                                fontFamily: 'Roboto',
-                                letterSpacing: 0.5,
-                                fontSize: 14,
-                            ),
-                        ),
-                    ),
-                    TextFormField(
-                        keyboardType: TextInputType.emailAddress,
-                        autofocus: false,
-                        decoration: InputDecoration(
-                            hintText: 'Email',
-                            fillColor: Colors.white,
-                            filled: true,
-                        ),
-                    ),
-                ],
-            ),
-        );
-
-        final password = Container(
-            child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                    Container(
-                        margin: EdgeInsets.only(bottom: 2),
-                        child: Text(
-                            'Password',
-                            textAlign: TextAlign.left,
-                            style: TextStyle(
-                                color: Colors.white,
-                                fontWeight: FontWeight.w800,
-                                fontFamily: 'Roboto',
-                                letterSpacing: 0.5,
-                                fontSize: 14,
-                            ),
-                        ),
-                    ),
-                    TextFormField(
-                        autofocus: false,
-                        obscureText: true,
-                        decoration: InputDecoration(
-                            hintText: 'Password',
-                            fillColor: Colors.white,
-                            filled: true,
-                        ),
-                    ),
-                ],
-            ),
-        );
-
-        final SignUpOrContinueButton = new Container(
-            child: new Column(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: <Widget>[
-                    Container(
-                        width: 144,
-                        height: 56,
-                        margin: EdgeInsets.only(bottom: 32),
-                        child: RaisedButton(
-                            onPressed: () {},
-                            color: Color(0xFF7986CB),
-                            child: Text('Giriş Yap', style: TextStyle(color: Colors.white)),
-                        ),
-                    ),
-                    new Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        children: <Widget>[
-                            Container(
-                                height: 56,
-                                width: 144,
-                                child: RaisedButton(
-                                    onPressed: () {},
-                                    color: Color(0xFF1A237E),
-                                    child: Text('Üye Değil Misin?\nKayıt Ol!',
-                                        textAlign: TextAlign.center,
-                                        style: TextStyle(color: Colors.white)),
-                                ),
-                                decoration: BoxDecoration(
-                                    color: Color(0xFF1A237E),
-                                    border: Border.all(
-                                        color: Colors.white,
-                                        width: 2.0,
-                                    ),
-                                ),
-                            ),
-                            Container(
-                                height: 56,
-                                width: 144,
-                                child: RaisedButton(
-                                    onPressed: () {},
-                                    color: Color(0xFF1A237E),
-                                    child: Text('Üye Olmadan\nDevam Et',
-                                        textAlign: TextAlign.center,
-                                        style: TextStyle(color: Colors.white)),
-                                ),
-                                decoration: BoxDecoration(
-                                    color: Color(0xFF1A237E),
-                                    border: Border.all(
-                                        color: Colors.white,
-                                        width: 2.0,
-                                    ),
-                                ),
-                            ),
-                        ]),
-                ],
-            ),
-        );
-
-        return Scaffold(
-            backgroundColor: Color(0xFF1A237E),
-            body: Center(
-                child: ListView(
-                    shrinkWrap: true,
-                    padding: EdgeInsets.only(left: 24.0, right: 24.0),
-                    children: <Widget>[
-                        logo,
-                        SizedBox(height: 32.0),
-                        email,
-                        SizedBox(height: 16.0),
-                        password,
-                        SizedBox(height: 32.0),
-                        SignUpOrContinueButton
-                    ],
-                ),
-            ),
-        );
+  @override
+  Widget build(BuildContext context) {
+    Widget _logo() {
+      return Container(
+        height: 200,
+        width: 180,
+        child: Image.asset('assets/logo.png'),
+      );
     }
+
+    Widget _email() {
+      return Container(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Container(
+              margin: EdgeInsets.only(bottom: 2),
+              child: Text(
+                'Email',
+                textAlign: TextAlign.left,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.w800,
+                  fontFamily: 'Palanquin',
+                  letterSpacing: 0.5,
+                  fontSize: 12,
+                ),
+              ),
+            ),
+            TextFormField(
+              keyboardType: TextInputType.emailAddress,
+              autofocus: false,
+              decoration: InputDecoration(
+                hintText: 'Email',
+                fillColor: Colors.white,
+                filled: true,
+              ),
+            ),
+          ],
+        ),
+      );
+    }
+
+    Widget _password() {
+      return Container(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Container(
+              margin: EdgeInsets.only(bottom: 2),
+              child: Text(
+                'Password',
+                textAlign: TextAlign.left,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.w800,
+                  fontFamily: 'Palanquin',
+                  letterSpacing: 0.5,
+                  fontSize: 12,
+                ),
+              ),
+            ),
+            TextFormField(
+              autofocus: false,
+              obscureText: true,
+              decoration: InputDecoration(
+                hintText: 'Password',
+                fillColor: Colors.white,
+                filled: true,
+              ),
+            ),
+          ],
+        ),
+      );
+    }
+
+    Widget _SignUpOrContinueButton() {
+      return Container(
+        child: new Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            Container(
+              width: 130,
+              height: 40,
+              margin: EdgeInsets.only(bottom: 32),
+              child: RaisedButton(
+                onPressed: () {},
+                color: GtuBtColors.selectedBarItemColor,
+                child: Text(
+                  'Giriş Yap',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.w800,
+                    fontFamily: 'Palanquin',
+                    letterSpacing: 0.5,
+                    fontSize: 16,
+                  ),
+                ),
+              ),
+            ),
+            new Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Container(
+                    height: 45,
+                    width: 135,
+                    child: RaisedButton(
+                      onPressed: () {},
+                      color: GtuBtColors.barBackgroundColor,
+                      child: Text(
+                        'Üye Değil Misin?\nKayıt Ol!',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.w800,
+                          fontFamily: 'Palanquin',
+                          letterSpacing: 0.5,
+                          fontSize: 12,
+                        ),
+                      ),
+                    ),
+                    decoration: BoxDecoration(
+                      color: GtuBtColors.barBackgroundColor,
+                      border: Border.all(
+                        color: Colors.white,
+                        width: 2.0,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    height: 45,
+                    width: 135,
+                    child: RaisedButton(
+                      onPressed: () {},
+                      color: GtuBtColors.barBackgroundColor,
+                      child: Text(
+                        'Üye Olmadan\nDevam Et',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.w800,
+                          fontFamily: 'Palanquin',
+                          letterSpacing: 0.5,
+                          fontSize: 12,
+                        ),
+                      ),
+                    ),
+                    decoration: BoxDecoration(
+                      color: GtuBtColors.barBackgroundColor,
+                      border: Border.all(
+                        color: Colors.white,
+                        width: 2.0,
+                      ),
+                    ),
+                  ),
+                ]),
+          ],
+        ),
+      );
+    }
+
+    return Scaffold(
+      backgroundColor: GtuBtColors.barBackgroundColor,
+      body: Center(
+        child: ListView(
+          shrinkWrap: true,
+          padding: EdgeInsets.only(left: 40.0, right: 40.0),
+          children: <Widget>[
+            _logo(),
+            SizedBox(height: 32.0),
+            _email(),
+            SizedBox(height: 16.0),
+            _password(),
+            SizedBox(height: 32.0),
+            _SignUpOrContinueButton()
+          ],
+        ),
+      ),
+    );
+  }
 }
