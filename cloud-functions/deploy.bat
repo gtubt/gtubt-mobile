7dde3749974627d2594563784bf57bee06569d7c
Yasir Nacak <yasir.nacak@gmail.com>
2020-03-19 14:18:49 +0300

    auto deploy scripts

---
+++ cloud-functions/deploy.bat
@@ -0,0 +1,1 @@
+gcloud functions deploy gtubt-api --runtime nodejs8 --trigger-http --entry-point app --allow-unauthenticated
---
+++ cloud-functions/deploy.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+gcloud functions deploy gtubt-api --runtime nodejs8 --trigger-http --entry-point app --allow-unauthenticated
