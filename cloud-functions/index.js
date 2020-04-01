459daa161f13d11ae43e4e52c171783a5c99c7e2
Yasir Nacak <yasir.nacak@gmail.com>
2020-04-01 21:13:06 +0300

    response structure fixed

--- cloud-functions/index.js
+++ cloud-functions/index.js
@@ -144,17 +144,46 @@ app.post('/create-user', jsonParser, (req, res, next) => {
 app.get('/get-user/:userEmail', (req, res, next) => {
     var userEmail = req.params.userEmail;
 
+    var responseStatus = 200;
+    var responseMessage = '';
+    var responseObj = null;
+
     const usersRef = db.collection('users');
     const getDoc = usersRef.where('email', '==', userEmail).get()
     .then(snapshot => {
         if (snapshot.empty) {
-            return res.status(404).send('Not Found');
+            // if user not found
+            responseStatus = 404;
+            responseMessage = 'User with given email not found';
+            responseObj = {
+                Body: null,
+                Message: responseMessage,
+                Code: responseStatus
+            }            
+            return res.status(responseStatus).json(responseObj);
+        } else {
+            // if user found
+            var responseBody = snapshot.docs[0].data();
+            responseStatus = 200;
+            responseMessage = 'User found';
+            responseObj = {
+                Body: responseBody,
+                Message: responseMessage,
+                Code: responseStatus
+            }
+            return res.status(responseStatus).json(responseObj);
         }
-
-        return res.send(snapshot.docs[0].data());
     }).catch(err => {
+        // an error occured while querying
+        responseStatus = 400;
+        responseMessage = 'There has been an error during the database query'
+        responseObj = {
+            Body: null,
+            Message: responseMessage,
+            Code: responseStatus
+        }
         console.log('Error getting document', err);
-        res.status(404).send('Not found');
+        res.status(responseStatus).json(responseObj);
     });
 });
 
