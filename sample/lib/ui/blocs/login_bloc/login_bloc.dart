5e2d5012a26157cfaea3ad69c3412af29bb60e42
Mehmet Gürol Çay <m.gurolcay@gmail.com>
2020-04-27 22:12:11 +0300

    feat: implement transformEvents

--- sample/lib/ui/blocs/login_bloc/login_bloc.dart
+++ sample/lib/ui/blocs/login_bloc/login_bloc.dart
@@ -1,3 +1,4 @@
+import 'package:rxdart/rxdart.dart';
 import 'package:GTUBT/ui/blocs/login_bloc/bloc.dart';
 import 'package:GTUBT/ui/utils/validators.dart';
 import 'package:bloc/bloc.dart';
@@ -6,6 +7,21 @@ class LoginBloc extends Bloc<LoginEvent, LoginState> {
   @override
   LoginState get initialState => LoginState.empty();
 
+  @override
+  Stream<LoginState> transformEvents(
+    Stream<LoginEvent> events,
+    Stream<LoginState> Function(LoginEvent) next,
+  ) {
+    final observableStream = events;
+    final nonDebounceStream = observableStream.where((event) {
+      return (event is! EmailChanged && event is! PasswordChanged);
+    });
+    final debounceStream = observableStream.where((event) {
+      return (event is EmailChanged || event is PasswordChanged);
+    }).debounceTime(Duration(milliseconds: 300));
+    return super
+        .transformEvents(nonDebounceStream.mergeWith([debounceStream]), next);
+  }
 
   @override
   Stream<LoginState> mapEventToState(LoginEvent event) async* {
@@ -20,17 +36,16 @@ class LoginBloc extends Bloc<LoginEvent, LoginState> {
   }
 
   Stream<LoginState> _mapEmailChangeToState(String email) async* {
-    yield currentState.update(isEmailValid: Validators.isValidEmail(email));
+    yield state.update(isEmailValid: Validators.isValidEmail(email));
   }
 
   Stream<LoginState> _mapPasswordChangeToState(String password) async* {
-    yield currentState.update(
-        isPasswordValid: Validators.isValidPassword(password));
+    yield state.update(isPasswordValid: Validators.isValidPassword(password));
   }
 
   Stream<LoginState> _mapLoginWithCredentialsPressedToState(
       String email, String password) async* {
-    yield currentState.update(
+    yield state.update(
       isEmailValid: Validators.isValidEmail(email),
       isPasswordValid: Validators.isValidPassword(password),
     );
