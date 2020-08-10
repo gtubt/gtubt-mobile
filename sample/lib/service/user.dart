9c913c5fd031d32e71ccd6b33eb8e2b310aa7a85
Ahmet Ergani <37360404+aErgani@users.noreply.github.com>
2020-08-10 21:08:36 +0300

    127 user service (#50)
    * Service is created and getUser method is implemented
    
    * Changed the name of method an added a forgotten path variable to request url
    
    * Fixed an error caused by uppercase 'N'
    
    * Created a test for get method.
    
    * field lastName was changed to lastname.
    model test was changed accordingly.
    post patch and delete methods for user has been implemented.
    user service test has been implemented
    
    Co-authored-by: ahmetergani <ahmtergn5@gmail.com>

---
+++ sample/lib/service/user.dart
@@ -0,0 +1,63 @@
+import 'package:GTUBT/models/user.dart';
+import 'package:GTUBT/models/api_response.dart';
+import 'package:http/http.dart' as http;
+import 'package:GTUBT/service/base.dart';
+import 'dart:convert';
+
+class UserService extends BaseService{
+
+   final servicePath = 'user';
+
+  Future<User> get(String email) async {
+    String url = '$baseUrl/$endpointPrefix/$servicePath/$email';
+    final response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      final apiResponse = ApiResponseSingle<User>.fromJson(json.decode(response.body));
+      if (apiResponse.status == 200) {
+        return apiResponse.body;
+      }
+    }
+  }
+
+  Future<http.Response> post(User user) async {
+    String url = '$baseUrl/$endpointPrefix/$servicePath';
+    var userJson = user.toJson();
+    var bodyData = json.encode(userJson);
+
+    final response = await http.post(
+      '$url',
+      headers: <String, String> {
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: bodyData
+    );
+
+    return response;
+  }
+
+  Future<http.Response> patch(User user) async {
+    var id = user.id;
+    String url = '$baseUrl/$endpointPrefix/$servicePath/$id';
+    var userJson = user.toJson();
+    var bodyData = json.encode(userJson);
+
+    final response = await http.patch(
+      '$url',
+      headers: <String, String> {
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: bodyData
+    );
+
+    return response;
+  }
+
+  Future<http.Response> delete(String id) async {
+    String url = '$baseUrl/$endpointPrefix/$servicePath/$id';
+
+    final response = await http.delete('$url');
+
+    return response;
+  }
+}
