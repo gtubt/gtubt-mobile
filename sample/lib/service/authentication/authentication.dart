da7775bdde55a90a39bc7b815f08d763e11de4b8
Mehmet Gürol Çay <m.gurolcay@gmail.com>
2020-08-30 16:42:13 +0300

    #96 firebase auth (#51)
    * Firebase Email&Password Auth.
    Gradle version 3.2.1 --> 3.3.1
    minSdkVersion 16 --> 21
    'multiDexEnabled...' added
    auth.dart file added.
    
    * feat: implement authentication bloc pattern
    
    * feat: implement login bloc pattern
    
    * feat: implement transformEvents
    
    * fix: upgrade equatable library
    
    * fix: gradle typo
    
    * fix: format
    
    * feat: implement login bloc to manage login
    
    * fixed user edit button
    
    * fix: connect sign up button and page
    
    * feat: implement register bloc
    
    * fix: added missing field
    
    * refactor: re-design sign up page
    
    * feat: add register bloc to main page bloc provider
    
    * feat: add context to manage navigation
    
    * feat: add snackBar to scaffold for giving information
    
    * feat: auth bloc have to cover app
    
    * todo: add todo
    
    * fix: firebase auth nullable problem
    
    * add todos
    
    Co-authored-by: Vefik Fırat Akman <vfakman@gmail.com>
    Co-authored-by: Halil Onur Çeçen <halilonur.cecen2016@gtu.edu.tr>

--- sample/lib/service/authentication/authentication.dart
+++ sample/lib/service/authentication/authentication.dart
@@ -3,6 +3,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 class AuthService {
   final FirebaseAuth _auth;
 
+//  TODO: implement singleton
   AuthService({FirebaseAuth firebaseAuth})
       : _auth = firebaseAuth ?? FirebaseAuth.instance;
 
@@ -26,8 +27,8 @@ class AuthService {
         (await _auth.createUserWithEmailAndPassword(
                 email: data['email'], password: data['password']))
             .user;
-    // TODO: create user
 
+    // TODO: create user
     return firebaseUser;
   }
 
