0b71a970f35ec80698a783d9406285d21f880462
ahmetergani <ahmtergn5@gmail.com>
2020-09-21 21:47:26 +0300

    Implemented test methods to try the update functionality

--- sample/test/service/event_test.dart
+++ sample/test/service/event_test.dart
@@ -2,13 +2,27 @@ import 'dart:convert';
 
 import 'package:GTUBT/service/event.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:GTUBT/models/api_response.dart';
 import 'package:GTUBT/models/event.dart';
+import 'package:http/http.dart';
 
 void main() {
-  test('Get Event with an ID', () async {
-    Event event = await EventService().get("Yehhjj8nYOLavI22uQpR");
+  test('Update Event with an ID', () async {
+    Event event = await EventService().get("JlhujcKnR5wpwbgTJRYe");
     expect(event == null, false);
-    expect(event.title, "Career Days");
+
+    event.title = "title";
+    
+    Response response = await EventService().patch(event);
+    expect(response == null, false);
+    expect(response.statusCode, 200);
+
+    event = await EventService().get("JlhujcKnR5wpwbgTJRYe");
+    expect(event == null, false);
+    expect(event.title, "Title");
+  });
+  test('Get Event with an ID', () async {
+    Event event = await EventService().get("JlhujcKnR5wpwbgTJRYe");
+    expect(event == null, false);
+    expect(event.title, "Title");
   });
 }
