25352b49f3f21d4cd9f0ac99c925119561e26fd3
H. İbrahim İLHAN <halil000ibrahim@gmail.com>
2020-11-13 18:43:55 +0300

    added ticket model and fixed qrcode overflow

--- sample/lib/ui/pages/ticket_popup.dart
+++ sample/lib/ui/pages/ticket_popup.dart
@@ -1,82 +1,85 @@
 import 'package:GTUBT/ui/style/color_sets.dart';
 import 'package:flutter/material.dart';
+import 'package:GTUBT/models/ticket.dart';
 
 // We need ticket model for here.
 // Usage: TicketPopup.showTicket(context, ticket);
 class TicketPopup {
-  static void showTicket(context, ticket){
-    String title = "Event Title";
-    String name = "Full Name";
-    String location = "Place";
-    String date = "01.01.2020";
-    String hour = "10:00";
-    String codeUrl = "";
+  static void showTicket(context, Ticket ticket){
+    String title = ticket.title;
+    String name = ticket.name;
+    String location = ticket.location;
+    String date = ticket.date;
+    String hour = ticket.hour;
+    String codeUrl =ticket.codeUrl;
     showDialog(
-      context: context,
-      builder: (context) {
-        return Dialog(
-          shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(20)), side: BorderSide(color: Color.fromRGBO(0, 0, 255, 1), width: 2)),
-          child: Padding(
-            padding: new EdgeInsets.all(20),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: <Widget>[
-                Text("Event", style: new TextStyle(color: ColorSets.barBackgroundColor, fontWeight: FontWeight.bold),),
-                Text(title, style: new TextStyle(color: ColorSets.profilePageThemeColor)),
-                Padding(
-                  padding: new EdgeInsets.only(top: 10, bottom: 10),
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: <Widget>[
-                      Row(
-                        mainAxisSize: MainAxisSize.min,
-                        children: <Widget>[
-                          Text("Attandee: ", style: new TextStyle(color: ColorSets.barBackgroundColor, fontWeight: FontWeight.bold),),
-                          Text(name, style: new TextStyle(color: ColorSets.profilePageThemeColor)),
-                        ],
-                      ),
-                      Row(
-                        mainAxisSize: MainAxisSize.min,
-                        children: <Widget>[
-                          Text("Location: ", style: new TextStyle(color: ColorSets.barBackgroundColor, fontWeight: FontWeight.bold), ),
-                          Text(location, style: new TextStyle(color: ColorSets.profilePageThemeColor)),
-                        ],
-                      ),
-                      Row(
-                        mainAxisSize: MainAxisSize.min,
-                        children: <Widget>[
-                          Text("Date: ", style: new TextStyle(color: ColorSets.barBackgroundColor, fontWeight: FontWeight.bold),),
-                          Text(date, style: new TextStyle(color: ColorSets.profilePageThemeColor)),
-                        ],
-                      ),
-                      Row(
-                        mainAxisSize: MainAxisSize.min,
-                        children: <Widget>[
-                          Text("Time: ", style: new TextStyle(color: ColorSets.barBackgroundColor, fontWeight: FontWeight.bold),),
-                          Text(hour, style: new TextStyle(color: ColorSets.profilePageThemeColor)),
-                        ],
-                      ),
-                      Padding(
-                        padding: new EdgeInsets.only(top: 10, bottom: 10,),
-                        child: DashSeperator(color: ColorSets.barBackgroundColor,),
-                      ),
-                      Image.network(codeUrl)
-                    ],)
-                )
-              ],
+        context: context,
+        builder: (context) {
+          return Dialog(
+            shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(20)), side: BorderSide(color: Color.fromRGBO(0, 0, 255, 1), width: 2)),
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: new EdgeInsets.all(20),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    Text("Event", style: new TextStyle(color: ColorSets.barBackgroundColor, fontWeight: FontWeight.bold),),
+                    Text(title, style: new TextStyle(color: ColorSets.profilePageThemeColor)),
+                    Padding(
+                        padding: new EdgeInsets.only(top: 10, bottom: 10),
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: <Widget>[
+                            Row(
+                              mainAxisSize: MainAxisSize.min,
+                              children: <Widget>[
+                                Text("Attandee: ", style: new TextStyle(color: ColorSets.barBackgroundColor, fontWeight: FontWeight.bold),),
+                                Text(name, style: new TextStyle(color: ColorSets.profilePageThemeColor)),
+                              ],
+                            ),
+                            Row(
+                              mainAxisSize: MainAxisSize.min,
+                              children: <Widget>[
+                                Text("Location: ", style: new TextStyle(color: ColorSets.barBackgroundColor, fontWeight: FontWeight.bold), ),
+                                Text(location, style: new TextStyle(color: ColorSets.profilePageThemeColor)),
+                              ],
+                            ),
+                            Row(
+                              mainAxisSize: MainAxisSize.min,
+                              children: <Widget>[
+                                Text("Date: ", style: new TextStyle(color: ColorSets.barBackgroundColor, fontWeight: FontWeight.bold),),
+                                Text(date, style: new TextStyle(color: ColorSets.profilePageThemeColor)),
+                              ],
+                            ),
+                            Row(
+                              mainAxisSize: MainAxisSize.min,
+                              children: <Widget>[
+                                Text("Time: ", style: new TextStyle(color: ColorSets.barBackgroundColor, fontWeight: FontWeight.bold),),
+                                Text(hour, style: new TextStyle(color: ColorSets.profilePageThemeColor)),
+                              ],
+                            ),
+                            Padding(
+                              padding: new EdgeInsets.only(top: 10, bottom: 10,),
+                              child: DashSeparator(color: ColorSets.barBackgroundColor,),
+                            ),
+                            Image.network(codeUrl,height: 130, width: 130,)
+                          ],)
+                    )
+                  ],
+                ),
+              ),
             ),
-          ),
-        );
-      } 
+          );
+        }
     );
   }
 }
 
-class DashSeperator extends StatelessWidget {
+class DashSeparator extends StatelessWidget {
   final double height;
   final Color color;
 
-  const DashSeperator({this.height = 1, this.color = Colors.black});
+  const DashSeparator({this.height = 1, this.color = Colors.black});
 
   @override
   Widget build(BuildContext context) {
