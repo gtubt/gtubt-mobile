2949a114efcb7594ccd4705ff3737dcbb769719a
ahmetergani <ahmtergn5@gmail.com>
2020-11-21 20:51:06 +0300

    Created and implemented a bloc, event and state for posts

---
+++ sample/lib/ui/blocs/post_bloc/post_bloc.dart
@@ -0,0 +1,29 @@
+import 'package:GTUBT/models/post.dart';
+import 'package:GTUBT/service/post.dart';
+import 'package:GTUBT/ui/blocs/post_bloc/post_event.dart';
+import 'package:GTUBT/ui/blocs/post_bloc/post_state.dart';
+//import 'package:GTUBT/ui/blocs/login_bloc/bloc.dart';
+//import 'package:GTUBT/ui/utils/validators.dart';
+import 'package:bloc/bloc.dart';
+
+class PostBloc extends Bloc<PostEvent, PostState> {
+  final PostService _postService = PostService();
+
+  @override
+  PostState get initialState => PostState.initial();
+
+  @override
+  Stream<PostState> mapEventToState(PostEvent event) async* {
+    if(event is LoadAllPosts){
+      var result = await _postService.getAll();
+      var postState = result.fold(
+        (postList) => PostState.success(postList),
+        (postFailure) => PostState.failure()
+      );
+      yield postState;
+    }
+    else{
+      yield this.initialState;
+    }
+  }
+}
