e4075cd921c84c69f08556ec3d9898fecbc7d351
ahmetergani <ahmtergn5@gmail.com>
2020-12-24 23:46:31 +0300

    Service method for sending reset password mail is implemented

--- gtubt/lib/service/authentication.dart
+++ gtubt/lib/service/authentication.dart
@@ -44,4 +44,8 @@ class AuthService {
   Future<auth.User> getUser() async {
     return _auth.currentUser;
   }
+
+   Future<void> sendPasswordResetEmail(String email) async {
+    return _auth.sendPasswordResetEmail(email: email);
+  }
 }
