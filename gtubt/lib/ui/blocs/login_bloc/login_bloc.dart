c169d9850475c484f4cc24fe89e6ec968d473302
ahmetergani <ahmtergn5@gmail.com>
2020-12-24 23:45:49 +0300

    mapping process of forgot password is implemented

--- gtubt/lib/ui/blocs/login_bloc/login_bloc.dart
+++ gtubt/lib/ui/blocs/login_bloc/login_bloc.dart
@@ -35,6 +35,9 @@ class LoginBloc extends Bloc<LoginEvent, LoginState> {
     } else if (event is LoginWithCredentialsPressed) {
       yield* _mapLoginWithCredentialsPressedToState(
           event.email, event.password);
+    }else if (event is ForgotPasswordPressed) {
+      yield* _mapForgotPasswordPressedToState(
+          event.email);
     }
   }
 
@@ -54,6 +57,16 @@ class LoginBloc extends Bloc<LoginEvent, LoginState> {
       yield LoginState.success();
     } catch (_) {
       yield LoginState.failure();
+    }
+  }
+
+  Stream<LoginState> _mapForgotPasswordPressedToState(String email) async* {
+    yield LoginState.loading();
+    try {
+      await _authService.sendPasswordResetEmail(email);
+      yield state.update(isPwRequestSent: true);
+    } catch (_) {
+      yield state.update(isPwRequestSent: false);
     }
   }
 }
