f64ad7d74a189f88084bc80f30a7f94528ef85e6
H. İbrahim İLHAN <halil000ibrahim@gmail.com>
2021-01-03 14:23:51 +0300

    added TextStyles

---
+++ gtubt/lib/ui/style/text_styles.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class TextStyles{
+  static const caption = TextStyle(fontSize: 12, fontWeight: FontWeight.w700);
+  static const subtitle2 = TextStyle(fontSize: 14, fontWeight: FontWeight.w700);
+  static const subtitle1 = TextStyle(fontSize: 16, fontWeight: FontWeight.w700);
+
+  static const bodyText1 = TextStyle(fontSize: 16, fontWeight: FontWeight.w300);
+
+  static const headline6 = TextStyle(fontSize: 20, fontWeight: FontWeight.w700);
+  static const headline5 = TextStyle(fontSize: 22, fontWeight: FontWeight.w700);
+  static const headline4 = TextStyle(fontSize: 28, fontWeight: FontWeight.w700);
+}
