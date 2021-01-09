b46299fe3d8072f1e45b260796bdd032c6c5268e
Mehmet Gürol Çay <m.gurolcay@gmail.com>
2021-01-09 16:19:12 +0300

    200: implement notification factory to show notifications in app

---
+++ gtubt/lib/ui/utils/notification.dart
@@ -0,0 +1,77 @@
+import 'package:flushbar/flushbar.dart';
+import 'package:flutter/material.dart';
+
+EdgeInsets getFlushbarMargin() {
+  return EdgeInsets.only(left: 8, right: 8, bottom: 8);
+}
+
+class NotificationFactory {
+  static Flushbar successFactory({
+    String title,
+    @required String message,
+  }) {
+    return Flushbar(
+      title: title,
+      message: message,
+      icon: Icon(Icons.info),
+      duration: Duration(seconds: 5),
+      margin: getFlushbarMargin(),
+      borderRadius: 8,
+    );
+  }
+
+  static Flushbar errorFactory({
+    String title,
+    @required String message,
+  }) {
+    return Flushbar(
+      title: title,
+      message: message,
+      icon: Icon(Icons.error),
+      duration: Duration(seconds: 5),
+      margin: getFlushbarMargin(),
+      borderRadius: 8,
+    );
+  }
+
+  static Flushbar warningFactory({
+    String title,
+    @required String message,
+  }) {
+    return Flushbar(
+      title: title,
+      message: message,
+      icon: Icon(Icons.error),
+      duration: Duration(seconds: 5),
+      margin: getFlushbarMargin(),
+      borderRadius: 8,
+    );
+  }
+
+  static Flushbar loadingFactory({
+    String title,
+    @required String message,
+  }) {
+    return Flushbar(
+      title: title,
+      message: message,
+      showProgressIndicator: true,
+      duration: Duration(seconds: 5),
+      margin: getFlushbarMargin(),
+    );
+  }
+
+  static Flushbar informationFactory({
+    String title,
+    @required String message,
+  }) {
+    return Flushbar(
+      title: title,
+      message: message,
+      icon: Icon(Icons.info),
+      duration: Duration(seconds: 5),
+      margin: getFlushbarMargin(),
+      borderRadius: 8,
+    );
+  }
+}
