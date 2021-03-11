c83fc7a7f1222ab9874e0af5f787ddee7613b982
yusuf erdogan <a.erdogan2018@gtu.edu.tr>
2021-03-11 15:58:08 +0300

    refactor blocs

--- gtubt/lib/ui/blocs/appbar_bloc/appbar_state.dart
+++ gtubt/lib/ui/blocs/appbar_bloc/appbar_state.dart
@@ -1,7 +1,7 @@
 import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
 
-class AppbarState extends Equatable{
+class AppbarState extends Equatable {
   final editMode;
 
   AppbarState({this.editMode = false});
@@ -15,8 +15,7 @@ class AppbarState extends Equatable{
   List<Object> get props => [];
 }
 
-class AppbarErrorState extends AppbarState{
-
+class AppbarErrorState extends AppbarState {
   @override
   List<Object> get props => [];
 }
