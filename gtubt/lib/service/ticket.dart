f9aa24ca09a21759aa95d92b0daf1be549a5aba0
Mehmet Gürol Çay <m.gurolcay@gmail.com>
2021-03-23 23:55:32 +0300

    230: refactor ticket service

--- gtubt/lib/service/ticket.dart
+++ gtubt/lib/service/ticket.dart
@@ -1,55 +1,51 @@
 import 'dart:convert';
 
+import 'package:GTUBT/exceptions/ticket.dart';
 import 'package:GTUBT/models/api_response.dart';
 import 'package:GTUBT/models/ticket.dart';
 import 'package:GTUBT/models/user.dart';
 import 'package:GTUBT/service/base.dart';
-import 'package:dartz/dartz.dart';
 import 'package:http/http.dart' as http;
-
-class TicketFailure {
-  final String message;
-
-  TicketFailure(this.message);
-
-  @override
-  String toString() => message;
-}
 
 class TicketService extends BaseService {
   final servicePath = 'ticket';
 
-  Future<Either<Ticket, TicketFailure>> get(User user) async {
+  Future<Ticket> get(User user) async {
     //  TODO: implement
   }
 
-  Future<Either<Ticket, TicketFailure>> post(User user) async {
+  Future<Ticket> post(User user) async {
     //  TODO: implement
   }
 
-  Future<Either<Ticket, TicketFailure>> patch(User user) async {
+  Future<Ticket> patch(User user) async {
     //  TODO: implement
   }
 
-  Future<Either<Ticket, TicketFailure>> delete(User user) async {
+  Future<Ticket> delete(User user) async {
     //  TODO: implement
   }
 
-  Future<Either<List<Ticket>, TicketFailure>> getAll(User user) async {
-    // TODO: refactor me
+  Future<List<Ticket>> getAll(User user) async {
+    // FIXME
     List<Ticket> _tickets = List();
 
     String url = '$baseUrl....';
     final response = await http.get('$url');
 
-    if (response.statusCode == 200) {
+    if (response.statusCode != 200) {
+      throw TicketException();
+    }
+    try {
       final apiResponse =
           ApiResponseList<Ticket>.fromJson(json.decode(response.body));
-      if (apiResponse.status == 200) {
-        _tickets = apiResponse.body;
-        return Left(_tickets);
+      if (apiResponse.status != 200) {
+        throw TicketException.message(apiResponse.message);
       }
+      _tickets = apiResponse.body;
+      return _tickets;
+    } catch (_) {
+      throw TicketException();
     }
-    return Right(TicketFailure('Fetching failure!'));
   }
 }
