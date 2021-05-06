a6c1ad8ddac58514ab227c8cae19ac626b8e223d
Ali Karadogan <akaradogan@outlook.com>
2021-05-07 01:34:44 +0300

    theme: Add primary color to main theme.

--- gtubt/lib/main.dart
+++ gtubt/lib/main.dart
@@ -6,6 +6,7 @@ import 'package:GTUBT/ui/blocs/calendar_bloc/bloc.dart';
 import 'package:GTUBT/ui/blocs/news_bloc/bloc.dart';
 import 'package:GTUBT/ui/blocs/register_bloc/bloc.dart';
 import 'package:GTUBT/ui/blocs/user_bloc/bloc.dart';
+import 'package:GTUBT/ui/style/color_sets.dart';
 import 'package:bloc/bloc.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
@@ -57,6 +58,7 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           // These will be customized for general app design
           primarySwatch: Colors.blue,
+          primaryColor: ColorSets.lightTextColor,
         ),
         routes: Routes.routes,
         initialRoute: ROOT_URL,
