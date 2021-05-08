9b9cc3175e7e548dbb95778afcdfd8cda78722b6
yusufferdogan <45846424+yusufferdogan@users.noreply.github.com>
2021-05-09 01:19:50 +0300

    Reimplement Signup Page (#108)
    * Reimplement Signup Page
    
    * changed snackBar to FlushBar
    
    * refactor sign up page
    
    * refactor
    
    Co-authored-by: Mehmet Gürol Çay <m.gurolcay@gmail.com>

--- gtubt/lib/ui/style/decorations.dart
+++ gtubt/lib/ui/style/decorations.dart
@@ -1,44 +1,50 @@
-import 'package:GTUBT/ui/style/color_sets.dart';
 import 'package:GTUBT/ui/style/text_styles.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
-class FormBoxContainer{
-  static InputDecoration textFieldStyle({String labelTextStr=""}) {
+class FormBoxContainer {
+  static InputDecoration textFieldStyle({String labelTextStr = ""}) {
     return InputDecoration(
       labelText: labelTextStr,
-      labelStyle: TextStyles.subtitle1.copyWith(color: ColorSets.profilePageThemeColor),
+      labelStyle: TextStyles.subtitle1.copyWith(color: Colors.white),
       contentPadding: EdgeInsets.only(left: 20),
       enabledBorder: OutlineInputBorder(
         borderRadius: BorderRadius.circular(10),
         borderSide: BorderSide(
-          color: ColorSets.profilePageThemeColor,
-          width: 4.0,
+          color: Colors.white,
+          width: 1.0,
         ),
       ),
       focusedBorder: OutlineInputBorder(
         borderRadius: BorderRadius.circular(10),
         borderSide: BorderSide(
-          color: ColorSets.selectedInputFieldColor,
-          width: 4.0,
+          color: Colors.white,
+          width: 1.0,
         ),
       ),
       disabledBorder: OutlineInputBorder(
         borderRadius: BorderRadius.circular(10),
         borderSide: BorderSide(
-          color: ColorSets.profilePageThemeColor,
-          width: 4.0,
+          color: Colors.white,
+          width: 1.0,
         ),
       ),
       border: OutlineInputBorder(
         borderRadius: BorderRadius.circular(10),
         borderSide: BorderSide(
-          color: ColorSets.profilePageThemeColor,
-          width: 4.0,
+          color: Colors.white,
+          width: 1.0,
         ),
       ),
     );
   }
+}
 
+class FormInputDecoration extends InputDecoration {
+  @override
+  Color get fillColor => Colors.white;
+  
+  @override
+  bool get filled => true;
 }
